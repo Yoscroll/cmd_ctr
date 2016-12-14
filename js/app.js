c28d76e5f59130eb9f60e57cc4d7c6de00608c6e
Yoscroll <ianhenry.c808@gmail.com>
2016-12-14 08:51:05 -1000

    finished

---
+++ js/app.js
@@ -0,0 +1,22 @@
+var box = document.getElementById('box');
+var box2 = document.getElementById('box2');
+var box3 = document.getElementById('box3');
+
+function red() {
+  box.style.backgroundColor="lightgreen";
+}
+
+function pink() {
+  box2.style.backgroundColor="salmon";
+}
+
+function blue() {
+  box3.style.backgroundColor="lightblue";
+}
+
+function leave(){
+  box.style.backgroundColor="white";
+  box2.style.backgroundColor="white";
+  box3.style.backgroundColor="white";
+}
+
