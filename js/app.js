5b4b20c2bc210426e03bcd0f2da5c0ee43250eee
Yoscroll <ianhenry.c808@gmail.com>
2016-12-14 18:16:23 -1000

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 var box = document.getElementById('box');
 var box2 = document.getElementById('box2');
 var box3 = document.getElementById('box3');
+var text = document.getElementsByClassName('text');
+var aBox = document.getElementsByClassName('aBox');
 
-function red() {
+function green() {
+  box.style.transition = "all 0.5s";
   box.style.backgroundColor="lightgreen";
   box.style.height = "200px";
   box.style.width = "200px";
 }
 
 function pink() {
+  box2.style.transition = "all 0.5s";
   box2.style.backgroundColor="salmon";
+  box2.style.height = "200px";
+  box2.style.width = "200px";
 }
 
 function blue() {
+  box3.style.transition = "all 0.5s";
   box3.style.backgroundColor="lightblue";
+  box3.style.height = "200px";
+  box3.style.width = "200px";
 }
 
 function leave(){
-  box.style.transition = "all 1s"; 
-  box2.style.transition = "all 1s"; 
-  box3.style.transition = "all 1s"; 
-  box.style.backgroundColor="white";
-  box2.style.backgroundColor="white";
-  box3.style.backgroundColor="white";
+  for (var i = 0; i < 3; i++) {
+    aBox[i].style.transition = "all 0.5s"; 
+    aBox[i].style.backgroundColor="white";
+    aBox[i].style.height = "100px";
+    aBox[i].style.width = "100px";
+  }
 }
 
